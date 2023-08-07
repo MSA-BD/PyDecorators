2501d392f592e869f958d970724f113125b8e5da
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-07 21:15:58 +0600

    Return a function from function

--- easyDecorators.py
+++ easyDecorators.py
@@ -2,3 +2,18 @@
 What is decorators in python?
 
 '''
+# def outer_fun(n):
+#     print(f"Im in outer function {n}")
+#     def inner_func(n):
+#         print(f"I'm in inner function {n}")
+#     inner_func(n)
+# outer_fun(1)
+
+def outer_fun(n):
+    print(f"Im in outer function {n}")
+    def inner_func():
+        return ("I'm in inner function")
+    return inner_func
+
+inner_value=outer_fun(1)
+print(inner_value)
