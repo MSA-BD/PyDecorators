acdd46e1c575f0b03dbbbe321dc0a7f465533a94
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-07 23:58:11 +0600

    Real multiple decorators

--- multipleDecorator.py
+++ multipleDecorator.py
@@ -5,12 +5,21 @@ def decorUppercaseFunc(fullNameFunc):
     return makeUpperCase
 def decorSplitFunc(fullNameFunc):
     def makeList():
-        return fullNameFunc().upper().split()
+        return fullNameFunc().split()
     return makeList
+@decorSplitFunc
+@decorUppercaseFunc
 def getFullName():
     return input("Enter first name: ")+" "+input("Enter last name: ")
+
+#multiple decorators
+print(getFullName())
 
 # capiName=decorUppercaseFunc(getFullName)()
 # print(capiName)
 # splitName=decorSplitFunc(getFullName)()
 # print(splitName)
+
+#eksathe duiti decorators
+# getName=decorSplitFunc(decorUppercaseFunc(getFullName))
+# print(getName())
