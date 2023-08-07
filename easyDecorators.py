0331a188b9359a75ba6168d4522d66e8198f5272
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-07 23:34:29 +0600

    Basic decor complete

--- easyDecorators.py
+++ easyDecorators.py
@@ -5,32 +5,32 @@ Accepting functions as argument and defining function inside other function.
 ***Decorators is a callable python object that is take other function as parametre and add some functionalities and
 return a function.***
 '''
-# def outer_fun(n):
-#     print(f"Im in outer function {n}")
-#     def inner_func(n):
-#         print(f"I'm in inner function {n}")
-#     inner_func(n)
-# outer_fun(1)
+def outer_fun(n):
+    print(f"Im in outer function {n}")
+    def inner_func(n):
+        print(f"I'm in inner function {n}")
+    inner_func(n)
+outer_fun(1)
 
-# def outer_fun(n):
-#     print(f"Im in outer function {n}")
-#     def inner_func():
-#         return ("I'm in inner function")
-#     return inner_func
+def outer_fun(n):
+    print(f"Im in outer function {n}")
+    def inner_func():
+        return ("I'm in inner function")
+    return inner_func
 #
-# inner_value=outer_fun(1)
-# print(inner_value())
+inner_value=outer_fun(1)
+print(inner_value())
 
-# def decoratorFunc(additionFunc):
-#     def innerFunc():
-#         additionValue=additionFunc()+float(input("Enter third number: "))
-#         return additionValue
-#     return innerFunc
-# def addition():
-#     return  float(input("Enter the first number: "))+float(input("Enter the first number: "))
-# innerFunc=decoratorFunc(addition)
-# finalValue=innerFunc()
-# print(finalValue)
+def decoratorFunc(additionFunc):
+    def innerFunc():
+        additionValue=additionFunc()+float(input("Enter third number: "))
+        return additionValue
+    return innerFunc
+def addition():
+    return  float(input("Enter the first number: "))+float(input("Enter the first number: "))
+innerFunc=decoratorFunc(addition)
+finalValue=innerFunc()
+print(finalValue)
 
 
 def addValue(x,y):
