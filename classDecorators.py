153ea1519ba9bb42a3df9b8438026fcf91bfabab
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-08 10:02:39 +0600

    More exploring class Decorators

--- classDecorators.py
+++ classDecorators.py
@@ -1,11 +1,24 @@
-class Add(object):
-    def __init__(self,func):
-        self.function=func
+class Divided(object):
+    def __init__(self,multiplyFunc):
+        self.multiply=multiplyFunc
     def __call__(self,a,b):
-        result=self.function(a,b)
-        return result**2
+        multiplyResult=self.multiply(a,b)
+        return multiplyResult/a/b
 
-@Add
+@Divided
+def multiply(x,y):
+    return x*y
+print(multiply(2,3)) #1
+
+
+class Multiply(object):
+    def __init__(self,additionFunc):
+        self.addition=additionFunc
+    def __call__(self,m,n):
+        additionResult=self.addition(m,n)
+        return additionResult*m*n
+
+@Multiply
 def add(a,b):
     return a+b
-print(add(3,1))
+print(add(3,4))
