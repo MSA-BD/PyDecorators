ce60ade6473e7a300c41deed2e5729de126b8dcd
MSA-BD <140373331+MSA-BD@users.noreply.github.com>
2023-08-11 17:30:36 +0600

    More closure example function

--- closure.py
+++ closure.py
@@ -1,10 +1,27 @@
-def makeCounter():
-    count=0
-    def increamentCounter():
+'''
+What is closure: A closure is a function-object and a closure is a
+reffernce to the enclosing scope.
+A closure is a function object that remembers values from it's
+enclosing scope even after the enclosing scope has been destroyed.
+'''
+# def makeCounter():
+#     count=0
+#     def increamentCounter():
+#         nonlocal count
+#         count+=1
+#         return count
+#     return increamentCounter
+# innerFunc=makeCounter()
+# print(innerFunc()) #1
+# print(innerFunc()) #2
+
+def makeCounter2(inValue):
+    def increamentCounter2():
         nonlocal count
-        count+=1
+        count+=inValue
         return count
-    return increamentCounter
-innerFunc=makeCounter()
-print(innerFunc()) #1
-print(innerFunc()) #2
+    count=0
+    return increamentCounter2
+innerFunc2=makeCounter2(10)
+print(innerFunc2()) #10
+print(innerFunc2()) #20
